9077ac0ff02f95bd0ea6587d68eb723fb0bdc083
Da111an <dallangrow@gmail.com>
2026-02-13 19:54:21 -0700

    added new photo and links

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -6,14 +6,45 @@ namespace Mission6.Controllers
 {
     public class HomeController : Controller
     {
+        // 1. Create a private variable to hold the database context
+        private MovieContext _context;
+
+        // 2. Add a Constructor so the controller can use the database
+        public HomeController(MovieContext temp)
+        {
+            _context = temp;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        // 3. Add the "Get to Know Joel" page action
+        public IActionResult GetToKnowJoel()
         {
             return View();
+        }
+
+        // 4. Add the GET action to show the movie form
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        // 5. Add the POST action to receive and save the movie data
+        [HttpPost]
+        public IActionResult AddMovie(Movie response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); // Add the new movie
+                _context.SaveChanges(); // Save changes to the SQLite database
+                return View("Index"); // Redirect to the home page after saving
+            }
+
+            return View(response); // If validation fails, stay on the form page
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Mission6/Program.cs
+++ Mission6/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Mission6.Models;
+
 namespace Mission6
 {
     public class Program
@@ -9,26 +12,34 @@ namespace Mission6
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // 1. Register the Database Context to use SQLite
+            builder.Services.AddDbContext<MovieContext>(options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("MovieConn"));
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
+            // 2. Ensure Static Files are enabled so your images in wwwroot/img work
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapStaticAssets();
+            // 3. Simplified routing for Mission 6
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
